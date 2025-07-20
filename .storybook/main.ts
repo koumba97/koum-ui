44679fd9769aa8d3489090d543634b0d41fb4513
Koumba KEITA <koumba.keita@epitech.eu>
2025-07-19 23:25:56 -0400

    Init project with Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "storybook";
+
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@chromatic-com/storybook",
+        "@storybook/addon-docs",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-a11y",
+        "@storybook/addon-vitest",
+    ],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {},
+    },
+    core: {
+        builder: "@storybook/builder-vite",
+    },
+    staticDirs: ["../public"],
+};
+
+export default config;
